43048c59cbe9f4050dda9c8fb7c5373fca29cd28
SebastianSL96 <sebastianleiva960929@gmail.com>
2025-11-08 22:01:33 -0500

    Nuevos cambios

---
+++ LPscript.js
@@ -0,0 +1,13 @@
+// Crear un programa en JavaScript que realice lo siguiente:
+// Debe solicitar al usuario 3 numeros por prompt y guardarlos en sus respectivas variables 
+
+// 1. Determinar numeros 
+
+let num1 = float(prompt ("Ingrese un numero que sea un numero real"));
+let num2 = float(prompt ("Ingrese un numero que sea un numero real"));
+let num3 = float(prompt ("Ingrese un numero que sea un numero real"));
+
+
+
+
+
