9c37a56be8b9498f050b67a267a7716eced40267
SebastianSL96 <sebastianleiva960929@gmail.com>
2025-11-08 22:04:59 -0500

    Nueva modificacion

--- LPscript.js
+++ LPscript.js
@@ -7,7 +7,18 @@ let num1 = float(prompt ("Ingrese un numero que sea un numero real"));
 let num2 = float(prompt ("Ingrese un numero que sea un numero real"));
 let num3 = float(prompt ("Ingrese un numero que sea un numero real"));
 
+if (num1 === num2 & num2 === num3) {
+    console.log ("Los numeros ingresados son iguales");
+} else {
+    let numeros = [ num1, num2, num3];
+    // De mayor a menor 
+    let descendente = [numeros].sort((a, b) => a - b);
+    // De menor a mayor 
+    let ascendente = [numeros].sort((a,b) => a - b);
 
+    console.log(`Ordenados de mayor a menor ${descendente}`);
+    console.log(`Ordenados de menor a mayor ${ascendente}`);
+}
 
 
 
